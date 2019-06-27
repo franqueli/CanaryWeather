f5a4160cc0fcfbeb3dd631ade017e8be195f1138
Franqueli Mendez <franqueli@gmail.com>
2019-06-27 16:37:13 -0400

    Get location

--- CanaryWeather/ViewController.m
+++ CanaryWeather/ViewController.m
@@ -7,16 +7,60 @@
 //
 
 #import "ViewController.h"
+#import <Corelocation/CoreLocation.h>
 
-@interface ViewController ()
+@interface ViewController () {
+    CLLocationCoordinate2D _locationCoordinate;
+}
 
+@property (nonatomic, strong) CLLocationManager *locationManager;
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void) viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+
+    // Use corelocation to get latitude and longitude
+    self.locationManager = [[CLLocationManager alloc] init];
+    _locationManager.delegate = self;
+}
+
+- (void) viewWillAppear: (BOOL)animated {
+    [super viewWillAppear: animated];
+
+    [self checkLocationAuthorization];
+}
+
+- (void) checkLocationAuthorization {
+    CLAuthorizationStatus authorizationStatus = CLLocationManager.authorizationStatus;
+    if (authorizationStatus != kCLAuthorizationStatusAuthorizedAlways
+            && authorizationStatus != kCLAuthorizationStatusAuthorizedWhenInUse) {
+        // Alert user that we need location to work
+        // Don't start the location manager if we are not authorized
+        NSLog(@"Alert user that location is required");
+        [_locationManager requestWhenInUseAuthorization];
+        return;
+    }
+
+    [_locationManager startUpdatingLocation];
+}
+
+
+#pragma mark - CLLocationManagerDelegate
+
+- (void) locationManager: (CLLocationManager *)manager didUpdateLocations: (NSArray<CLLocation *> *)locations {
+    CLLocation *lastLocation = locations.lastObject;
+    _locationCoordinate = lastLocation.coordinate;
+
+    NSLog(@"*** Latitude: %f Longitude: %f ***", _locationCoordinate.latitude, _locationCoordinate.longitude);
+    [manager stopUpdatingLocation];
+    // TODO: Stop spinner
+    // TODO: Get location name from Coordinates
+}
+
+- (void) locationManager: (CLLocationManager *)manager didChangeAuthorizationStatus: (CLAuthorizationStatus)status {
+    [self  checkLocationAuthorization];
 }
 
 
