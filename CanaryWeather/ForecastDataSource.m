da256fcaada0a86f2e74f33d9d547194b38873be
Franqueli Mendez <franqueli@gmail.com>
2019-06-27 19:12:19 -0400

    Call api

--- CanaryWeather/ForecastDataSource.m
+++ CanaryWeather/ForecastDataSource.m
@@ -9,7 +9,7 @@
 #import <Foundation/Foundation.h>
 #import "ForecastDataSource.h"
 
-static NSString *const FORCAST_URL_FORMAT = @"https://api.darksky.net/forecast/%@/%.4f,%.4f?exclude=minutely,alerts,flags,hourly";
+static NSString *const FORECAST_URL_FORMAT = @"https://api.darksky.net/forecast/%@/%.4f,%.4f?exclude=minutely,alerts,flags,hourly";
 
 @interface ForecastDataSource()
 
@@ -23,12 +23,31 @@ static NSString *const FORCAST_URL_FORMAT = @"https://api.darksky.net/forecast/%
     longitude = -74.0060;
 
     // TODO Get API Key from elsewhere, maybe a info plist key
-    NSString *forecastURLString = [NSString stringWithFormat: FORCAST_URL_FORMAT, @"", latitude, longitude];
+    NSString *forecastURLString = [NSString stringWithFormat: FORECAST_URL_FORMAT, @"", latitude, longitude];
 
     NSURL *forecastURL = [[NSURL alloc] initWithString: forecastURLString];
 
     NSLog(@"URL: %@", forecastURL);
+    // FIXME specify application/json accept header
+    NSURLSessionDataTask *task = [[NSURLSession sharedSession] dataTaskWithURL: forecastURL completionHandler: ^(NSData *data, NSURLResponse *response, NSError *error) {
+        NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *)response;      // TODO add check for type
 
+        if (error) {
+            // TODO: Call back failed on delegate
+            NSLog(@"*** Forecast API Failed: %@", error.localizedDescription);
+            return;
+        }
+
+        if ([httpResponse statusCode] != 200) {
+            NSLog(@"*** Forecast API Failed Code: %ld", (long)[httpResponse statusCode]);
+            // TODO: Call back failed on delegate
+            return;
+        }
+
+        NSLog(@"*** Data: %@ ***", [[NSString alloc] initWithData: data encoding: NSUTF8StringEncoding]);
+    }];
+
+    [task resume];
 }
 
 
